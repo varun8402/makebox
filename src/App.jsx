1dd2325d6b02052b7e356f1965e846f621e2b41b
Varun-047 <varunmarwah.2004@gmail.com>
2025-09-26 23:47:51 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import "./App.css";
 import { ThreeDModel } from "./components/3dmodel";
 import { BlueSwitch } from "./components/blueswitchmodel";
 import HeroSection from "./screens/HeroSection";
+import Navbar from "./components/Nav";
 export default function App() {
   return (
     <>
+      <Navbar />
       <HeroSection/>
       <BlueSwitch />
     </>
