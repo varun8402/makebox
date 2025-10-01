de0057776c3858ccdf6fa87af380db73fce93806
Varun-047 <varunmarwah.2004@gmail.com>
2025-10-01 16:36:22 +0530

    Added links

--- src/screens/Footer.jsx
+++ src/screens/Footer.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="w-full bg-[#D9D9D9] text-black py-8 px-4 sm:px-8 md:px-16 flex items-center ">
-      {/* Left and Middle Group */}
       <div className="flex items-center gap-8 md:gap-12">
-        {/* Logo */}
         <div>
           <img 
             src="https://res.cloudinary.com/dwdsw96fy/image/upload/v1759086405/MAKEBOX_LOGO_znnits.png" 
@@ -14,20 +12,18 @@ const Footer = () => {
           />
         </div>
 
-        {/* Divider and Middle Section */}
         <div className="flex items-center border-l-2 border-black pl-8 md:pl-12 h-40">
           <div className="flex flex-col sm:flex-row gap-y-4 sm:gap-x-12 md:gap-x-16">
-            {/* Column 1 */}
             <div className="flex flex-col space-y-3">
               <p className="text-xs font-semibold tracking-widest whitespace-nowrap">ABHIMANYU PANNU (DESIGN)</p>
-              <a href="#" className="text-xs underline font-bold tracking-widest">INSTAGRAM</a>
-              <a href="#" className="text-xs underline font-bold tracking-widest">BEHANCE</a>
+              <a href="https://www.instagram.com/afterthought.flac.raw/" className="text-xs underline font-bold tracking-widest">INSTAGRAM</a>
+              <a href="https://www.behance.net/fitoore1" className="text-xs underline font-bold tracking-widest">BEHANCE</a>
             </div>
             {/* Column 2 */}
             <div className="flex flex-col space-y-3">
               <p className="text-xs font-semibold tracking-widest whitespace-nowrap">VARUN MARWAH (FRONT-END DEV.)</p>
-              <a href="#" className="text-xs underline font-bold tracking-widest">LINKEDIN</a>
-              <a href="#" className="text-xs underline font-bold tracking-widest">GITHUB</a>
+              <a href="https://www.linkedin.com/in/varun-marwah/" className="text-xs underline font-bold tracking-widest">LINKEDIN</a>
+              <a href="https://github.com/varun8402" className="text-xs underline font-bold tracking-widest">GITHUB</a>
             </div>
           </div>
         </div>
